1e93e79eb1254af6bf0d74b2f7d12b7f0cd66433
Vincent Prat <vpratfr@gmail.com>
2024-09-19 11:09:02 +0200

    Create french.js

---
+++ src/editor/locales/french.js
@@ -0,0 +1,20 @@
+module.exports = {
+    locale_name: 'Français',
+    locale_shorthand: ['fr', 'french'],
+    // Translations:
+    justifyCenter: 'Centré',
+    justifyLeft: 'Justifié à gauche',
+    justifyRight: 'Justifé à droite',
+    bold: 'Gras',
+    code: 'Code',
+    headings: 'Titres (h1-h6)',
+    link: 'Lien',
+    image: 'Insérer une image',
+    italic: 'Italique',
+    orderedList: 'Liste numérotée (1, 2, 3, ...)',
+    unorderedList: 'Liste à puces',
+    removeFormat: 'Supprimer la mise en forme.\nEnlève les titres, gras, italique, etc.',
+    separator: null,
+    table: 'Insérer un tableau',
+    underline: 'Souligné'
+}
